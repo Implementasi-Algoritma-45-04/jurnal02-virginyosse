d2c13d53aa79417e1a8ba5d5c942199274d273ea
virginyosse <virginyosse01@gmail.com>
2021-10-01 15:52:08 +0700

    Solusi Jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main (String [] args) {
+	System.out.println("NIM   : 6706213117");
+	System.out.println("KELAS : D3 RPLA 4504");
+	System.out.println("NAMA  : VIRGIN YOSSE");
+	}
+}
